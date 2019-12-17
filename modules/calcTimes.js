93064b431664cface82d3d85c2878b7bb6634b31
Munieru <20086673+munierujp@users.noreply.github.com>
2019-12-18 01:30:40 +0900

    Add feature: save records

--- modules/calcTimes.js
+++ modules/calcTimes.js
@@ -10,30 +10,12 @@ function calcTimes ({ stayingTime, breakTime, standardWorkingTime, workingTimeUn
   const standardWorkingTimeDuration = newDurationFromHTMLTime(standardWorkingTime)
   const overtimeDuration = workingTimeDuration.minus(standardWorkingTimeDuration)
   return {
-    stayingTime: {
-      value: stayingTime,
-      duration: stayingTimeDuration
-    },
-    breakTime: {
-      value: breakTime,
-      duration: breakTimeDuration
-    },
-    actualWorkingTime: {
-      value: toHTMLTime(actualWorkingTimeDuration),
-      duration: actualWorkingTimeDuration
-    },
-    workingTime: {
-      value: toHTMLTime(workingTimeDuration),
-      duration: workingTimeDuration
-    },
-    standardWorkingTime: {
-      value: standardWorkingTime,
-      duration: standardWorkingTimeDuration
-    },
-    overtime: {
-      value: toHTMLTime(overtimeDuration),
-      duration: overtimeDuration
-    }
+    stayingTime,
+    breakTime,
+    actualWorkingTime: toHTMLTime(actualWorkingTimeDuration),
+    workingTime: toHTMLTime(workingTimeDuration),
+    standardWorkingTime,
+    overtime: toHTMLTime(overtimeDuration)
   }
 }
 
