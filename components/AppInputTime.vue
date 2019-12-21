22197786c29aef3e4f3cd52dc885af597a387d3d
Munieru <20086673+munierujp@users.noreply.github.com>
2019-12-21 21:22:08 +0900

    Add AppInputTime.vue

---
+++ components/AppInputTime.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-text-field
+    v-model="_value"
+    :rules="rules"
+    :label="label"
+    type="time"
+  />
+</template>
+
+<script>
+import isHTMLTime from '~/modules/isHTMLTime'
+
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      rules: [
+        value => isHTMLTime(value) || this.$t('ERROR_MESSAGE_NOT_TIME')
+      ]
+    }
+  },
+  computed: {
+    _value: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
