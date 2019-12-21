d0fdc692f6fade58928376a57ef80b03a33f439d
Munieru <20086673+munierujp@users.noreply.github.com>
2019-12-21 17:10:36 +0900

    Add AppIconButton.vue

---
+++ components/AppIconButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-btn
+    @click="click"
+    icon
+  >
+    <v-icon>{{ icon }}</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    click (e) {
+      this.$emit('click', e)
+    }
+  }
+}
+</script>
